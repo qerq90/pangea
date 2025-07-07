ac2c01124e7a4e2c9d319252625499ab1a1ca84b
qerq90 <alsavidi37@gmail.com>
2025-07-07 13:42:05 +0300

    fix snake case

--- app/src/main/scala/server/model/VkChecking.scala
+++ app/src/main/scala/server/model/VkChecking.scala
@@ -1,12 +1,19 @@
 package server.model
 
+import io.circe.generic.extras.Configuration
+import io.circe.generic.extras.semiauto.{
+  deriveConfiguredDecoder,
+  deriveConfiguredEncoder
+}
 import io.circe.{Decoder, Encoder}
-import io.circe.generic.semiauto.{deriveDecoder, deriveEncoder}
 
 case class VkChecking(`type`: String, groupId: Long)
 
 object VkChecking {
 
-  implicit val decoder: Decoder[VkChecking] = deriveDecoder
-  implicit val encoder: Encoder[VkChecking] = deriveEncoder
+  implicit val configuration: Configuration =
+    Configuration.default.withSnakeCaseMemberNames
+
+  implicit val decoder: Decoder[VkChecking] = deriveConfiguredDecoder
+  implicit val encoder: Encoder[VkChecking] = deriveConfiguredEncoder
 }
