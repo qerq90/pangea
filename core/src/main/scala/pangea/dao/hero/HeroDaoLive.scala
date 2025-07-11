8270b80f50b8c18e917e1a1ba0d541f453c06b65
qerq90 <alsavidi37@gmail.com>
2025-07-11 23:01:14 +0300

    change methods to return made hero and return made user(but id)

--- core/src/main/scala/pangea/dao/hero/HeroDaoLive.scala
+++ core/src/main/scala/pangea/dao/hero/HeroDaoLive.scala
@@ -1,6 +1,7 @@
 package pangea.dao.hero
 
 import doobie.implicits._
+import doobie.postgres.circe.json.implicits._
 import doobie.util.transactor.Transactor
 import io.circe.syntax.EncoderOps
 import pangea.model.hero.Hero
@@ -17,7 +18,7 @@ class HeroDaoLive(xa: Transactor[Task]) extends HeroDao {
       .transact(xa)
 
   override def insertHero(hero: Hero): Task[Hero] =
-    sql"insert into heroes(user_id, state, base_stats, fight_stats, equipment) values(${hero.userId}, ${hero.state}, ${hero.baseStats.asJson.noSpaces}, ${hero.fightStats.asJson.noSpaces}, ${hero.equipment.asJson.noSpaces}) returning id, user_id, state, base_stats, fight_stats, equipment".update
+    sql"insert into heroes(user_id, state, base_stats, fight_stats, equipment) values(${hero.userId}, ${hero.state}, ${hero.baseStats.asJson}, ${hero.fightStats.asJson}, ${hero.equipment.asJson}) returning id, user_id, state, base_stats, fight_stats, equipment".update
       .withUniqueGeneratedKeys[Hero](
         "id",
         "user_id",
