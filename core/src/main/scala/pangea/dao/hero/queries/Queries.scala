0474b2d26c4920401e0f65ca529bc4eb6108a8f1
qerq90 <alsavidi37@gmail.com>
2025-09-13 19:26:55 +0300

    add hero stats scene and proceed to it

--- core/src/main/scala/pangea/dao/hero/queries/Queries.scala
+++ core/src/main/scala/pangea/dao/hero/queries/Queries.scala
@@ -13,6 +13,8 @@ import pangea.model.user.UserId
 object Queries {
   private val fields: Fragment =
     sql"id, user_id, state, lvl, exp, upgrade_points, race, base_stats, fight_stats, equipment"
+  private val fieldInsert: Fragment =
+    sql"user_id, state, lvl, exp, upgrade_points, race, base_stats, fight_stats, equipment"
   private val tableName: Fragment = sql"heroes"
 
   private val selectAll = fr"select $fields from $tableName"
@@ -21,7 +23,7 @@ object Queries {
     selectAll ++ sql"where user_id = $userId"
 
   def insert(hero: Hero): Fragment =
-    sql"insert into $tableName($fields) values(${hero.userId}, ${hero.state}, ${hero.lvl}, ${hero.exp}, ${hero.upgradePoints}, ${hero.race}, ${hero.baseStats.asJson}, ${hero.fightStats.asJson}, ${hero.equipment.asJson})"
+    sql"insert into $tableName($fieldInsert) values(${hero.userId}, ${hero.state}, ${hero.lvl}, ${hero.exp}, ${hero.upgradePoints}, ${hero.race}, ${hero.baseStats.asJson}, ${hero.fightStats.asJson}, ${hero.equipment.asJson})"
 
   def updateRace(userId: UserId, race: Race): Fragment =
     sql"update $tableName set race = $race where user_id = $userId"
