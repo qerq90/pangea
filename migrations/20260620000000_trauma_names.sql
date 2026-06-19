16a84a40950ae2c88e7768b5792db42899289cf6
qerq90 <alsavidi37@gmail.com>
2026-06-20 01:32:25 +0300

    забыли аннотации в миграциях

--- migrations/20260620000000_trauma_names.sql
+++ migrations/20260620000000_trauma_names.sql
@@ -1,4 +1,13 @@
--- Replace single trauma_name with trauma_names (comma-separated list)
+-- +goose Up
+-- +goose StatementBegin
 ALTER TABLE heroes ADD COLUMN trauma_names TEXT NOT NULL DEFAULT '';
 UPDATE heroes SET trauma_names = COALESCE(trauma_name, '');
 ALTER TABLE heroes DROP COLUMN trauma_name;
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+ALTER TABLE heroes ADD COLUMN trauma_name VARCHAR;
+UPDATE heroes SET trauma_name = NULLIF(trauma_names, '');
+ALTER TABLE heroes DROP COLUMN trauma_names;
+-- +goose StatementEnd
